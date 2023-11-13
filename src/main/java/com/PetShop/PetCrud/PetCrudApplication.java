2c5f2395e082201159ef7255867064efa0257d60
Felipe Pereira <felipejiti@hotmail.com>
2023-11-13 16:55:12 -0300

    Project criated

---
+++ src/main/java/com/PetShop/PetCrud/PetCrudApplication.java
@@ -0,0 +1,13 @@
+package com.PetShop.PetCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PetShop/PetCrud/PetCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PetShop.PetCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
